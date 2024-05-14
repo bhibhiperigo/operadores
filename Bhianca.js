34e0cf5b935ba6e9a142407a5da3aaf7bc11ad6c
bhibhiperigo <135250045+bhibhiperigo@users.noreply.github.com>
2024-05-14 08:48:20 -0300

    Add files via upload

---
+++ Bhianca.js
@@ -0,0 +1,40 @@
+let nome= "Bhianca"
+//Bhianca
+var idade= 16
+//16
+const soma= (nome+idade)
+//Bhianca 16
+
+var anos=2007
+let multiplicação=(idades*anos)
+//32.112
+
+let idade=16
+let nome="Bhianca"
+//Bhianca 16 
+
+var meses= 12
+let divisão= (anos/meses)
+//165,25
+
+let nome= "Bhianca"
+let sobrenome="Muldenberger"
+const adição=(nome+sobrenome)
+//Bhianca Muldenberger"
+
+let nome="bhianca"
+let data= "nasceu dia"
+var dia= 06/06
+const mais=(nome+data+dia)
+//Bhianca nasceu dia 06/06
+
+let dia=06
+var mês= 06
+const subtração= (dia-mês)
+//0
+
+var comp={dia==mes}
+//true
+
+var comp={idade==anos}
+//False
